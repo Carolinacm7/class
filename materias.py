c34ae2fd20d8d2ae54e5ea838fd01345a75f67e6
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-03-11 16:00:23 -0500

    [new]code

---
+++ materias.py
@@ -0,0 +1,21 @@
+""" Escribir un script que guarde las materias de un colegio 
+(por ejemplo Inglés, Física, Sociales, Historia y Programación) 
+en una lista y la muestre por pantalla el mensaje Yo estoy viendo <materia>,
+ donde <materia> es cada una de las materias de la lista.
+  utilice una función llamada yo estoy viendo,
+   que reciba como entrada una cadena de texto con los nombres
+    de las materias separadas por coma.
+ Ej: "Inglés,Física, Sociales,Historia,Programación" """
+
+#resultado ['Yo estoy viendo Inglés', 'Yo estoy viendo Física', 'Yo estoy viendo  Sociales', 'Yo estoy viendo Historia', 'Yo estoy viendo Programación']
+#print(yoEstoyViendo("Inglés,Física, Sociales,Historia,Programación"))
+
+def yoEstoyViendo (materia):
+    list.append[""]
+    lista=str("Inglés,Física, Sociales,Historia,Programación")
+
+    materia=lista.split(",") 
+    
+    return yoEstoyViendo(materia)
+
+print(yoEstoyViendo(materia))
